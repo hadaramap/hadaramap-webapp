ca3a20dbddccbf3a2b2d6c98e75a40c9b93ca8d9
Mohamed Abdoul Karim DIOP [IPD DDSI/DIF] <mohamedabdoulkarim.diop@pasteur.sn>
2024-11-24 14:47:15 +0000

    Integrate translation service and User registration API --in-progress

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,27 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
+  new TranslateHttpLoader(http, './i18n/', '.json');
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    importProvidersFrom([BrowserAnimationsModule])
+    importProvidersFrom([BrowserAnimationsModule]),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideTranslateService({
+      defaultLanguage: 'fr',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
+     })
   ],
 };
--- src/app/components/menu-top-bar/menu-top-bar.component.ts
+++ src/app/components/menu-top-bar/menu-top-bar.component.ts
@@ -1,16 +1,35 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslateDirective, TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { ButtonModule } from 'primeng/button';
 import { OverlayPanelModule } from 'primeng/overlaypanel';
 @Component({
   selector: 'app-menu-top-bar',
   standalone: true,
-  imports: [RouterLink, ButtonModule, NgOptimizedImage, CommonModule, RouterLinkActive, OverlayPanelModule],
+  imports: [
+    RouterLink,
+    ButtonModule,
+    NgOptimizedImage,
+    CommonModule,
+    RouterLinkActive,
+    OverlayPanelModule,
+    TranslatePipe,
+    TranslateDirective,
+  ],
 
   templateUrl: './menu-top-bar.component.html',
   styleUrl: './menu-top-bar.component.scss',
 })
 export class MenuTopBarComponent {
   isHomeMenu = input<boolean>(true);
+  languages = ['fr', 'en'];
+  currentLanguage?: string;
+  constructor(private translate: TranslateService) {
+    this.currentLanguage = translate.currentLang;
+  }
+
+  changeLanguage(language: string) {
+    this.translate.use(language);
+  }
 }
